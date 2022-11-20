c2e18a09c9f5a16be4bc5616c4880952cbcc2477
minshantt <min.shantt@gmail.com>
2022-11-20 23:43:10 +0700

    add-Layout-Menu-Navbar-Wrapper-VidCard

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex: 1;
+  height: 100vh;
+  background-color: #202020;
+  color: white;
+`;
+
+const Menu = () => {
+  return <Container>Menu</Container>;
+};
+
+export default Menu;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 3px solid salmon;
+`;
+
+const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
+
+export default Navbar;
